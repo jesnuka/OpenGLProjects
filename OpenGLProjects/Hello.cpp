e3f1451727506f2feb9b403a4a55a9080b3bded9
Jesse Nukarinen <jesse.nukarinen@outlook.com>
2022-08-25 12:52:09 +0300

    Create Visual Studio 2019 Project. Add Include and Library references. Add Hello.cpp and Hello.h, with functionality to draw an empty window with GLFW and OpenGL.

---
+++ OpenGLProjects/Hello.cpp
@@ -0,0 +1,48 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "Hello.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+int main()
+{
+	// Initialize GLFW
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+	// Create the window object
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Learning OpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW Window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	// Set Window resizing function callback
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Render Loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
---
+++ OpenGLProjects/Hello.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// Resize the window
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
